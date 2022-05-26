6298acc5dbe3c437cd75829900cc2739c538362d
dglalperen <alperen.dagli43@gmail.com>
2022-05-26 18:12:04 +0200

    Concat Nodes on Click

--- src/Testing/Test.js
+++ src/Testing/Test.js
@@ -3,6 +3,7 @@ import ForceGraph2D from "react-force-graph-2d";
 import { companyByQid } from "../Queries/queries";
 import { useEffect, useState } from "react";
 import { randomColor } from "../Helpers/generateColor";
+import { click } from "@testing-library/user-event/dist/click";
 
 const Test = () => {
   // 4. onClick --> Daten bearbeiten (Arrays kollidieren) und wieder an Forcegraph übergeben
@@ -18,6 +19,8 @@ const Test = () => {
     "companyByQid"
   );
 
+  const [clickCounter, setClickCounter] = useState(0);
+
   // 2. Daten bearbeiten --> MainNode erstellen in ein State, Country, CEO, (Subsidiary)
   const [countriesNode, setCountriesNode] = useState([]);
 
@@ -27,42 +30,65 @@ const Test = () => {
     links: [],
   });
 
+  const handleNodeClick = (node, event) => {
+    // Append new nodes to nodes State
+    if (node.__typename === "company") {
+      if (data.country && clickCounter === 0) {
+        const countryNodes = [];
+        const countryLinks = [];
+        data.country.forEach((country) => {
+          countryNodes.push({
+            id: country.qid,
+            name: country.label,
+            __typename: "country",
+            color: "green",
+            sizeInPx: 5,
+          });
+
+          countryLinks.push({ source: node.id, target: country.qid });
+        });
+
+        const ceoNodes = [];
+        const ceoLinks = [];
+        data.ceo.forEach((ceo) => {
+          ceoNodes.push({
+            id: ceo.qid,
+            name: ceo.label,
+            __typename: "ceo",
+            color: "yellow",
+            sizeInPx: 5,
+          });
+
+          ceoLinks.push({ source: node.id, target: ceo.qid });
+        });
+
+        const oldNodes = countryNodes.concat(nodes.nodes);
+        const newNodes = oldNodes.concat(ceoNodes);
+
+        const newLinks = countryLinks.concat(ceoLinks);
+        //setNodes({ ...nodes, nodes: newNodes });
+        setNodes({ nodes: newNodes, links: newLinks });
+        setClickCounter(1);
+      }
+    }
+  };
+
   useEffect(() => {
-    console.clear();
-    // Pushing countries to countriesNode
-    const currentCountryNodes = [];
-    const currentCountryLinks = [];
-    const origin = {
-      id: data.qid,
-      name: data.label,
-      __typename: data.type,
-      color: "blue",
-      sizeInPx: 6,
-    };
-
-    if (data.country) {
-      data.country.forEach((country) => {
-        currentCountryNodes.push({
-          id: country.qid,
-          name: country.label,
-          __typename: "country",
-          color: "red",
-          sizeInPx: 5,
-        });
-
-        currentCountryLinks.push({
-          source: nodes.id,
-          target: country.id,
-        });
-      });
-      setCountriesNode(currentCountryNodes);
+    if (data.qid) {
       setNodes({
-        nodes: currentCountryNodes.concat(origin),
-        links: currentCountryLinks,
+        nodes: [
+          {
+            id: data.qid,
+            name: data.label,
+            __typename: data.type,
+            color: "blue",
+            sizeInPx: 6,
+          },
+        ],
+        links: [],
       });
     }
-    console.log(nodes);
-  }, []);
+  }, [data]);
 
   return (
     <div className="test">
@@ -78,7 +104,11 @@ const Test = () => {
           nodeColor={"color"}
           nodeLabel={"name"}
           nodeVal={"sizeInPx"}
+          linkColor={"black"}
+          linkWidth={5}
+          linkDirectionalParticles={2}
           graphData={nodes}
+          onNodeClick={handleNodeClick}
         />
       )}
     </div>
--- src/Testing/useFetchTest.js
+++ src/Testing/useFetchTest.js
@@ -1,32 +1,31 @@
 import { useState, useEffect } from "react";
 
-const useFetchTest = (url,queryWithBackTicks,dataUnwrap) => {
-    const [someData,setSomeData] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null)
-    const QUERY = queryWithBackTicks
+const useFetchTest = (url, queryWithBackTicks, dataUnwrap) => {
+  const [someData, setSomeData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const QUERY = queryWithBackTicks;
 
-    useEffect(() => {
-        setIsLoading(true)
-        fetch(url, {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({ query: QUERY})
-        })
-        .then(response => response.json())
-        .then(data => {
-            setSomeData(data["data"][dataUnwrap])
-            setIsLoading(false)
-            setError(null)
-        })
-        .catch(err => {
-            setIsLoading(false)
-            setError(err.message)
-        })
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query: QUERY }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setSomeData(data["data"][dataUnwrap]);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err.message);
+      });
+  }, [url]);
 
-    }, [url]) 
-
-    return {someData,isLoading,error}
-}
+  return { someData, isLoading, error };
+};
 
 export default useFetchTest;
